6be3bf4b07e2f9e2e76031a164560eb599a87342
Miranda M <miranda.esmer.50@gmail.com>
2024-06-10 18:02:06 -0600

    add malformed genre errors and manual tests

--- spotify_deduper.py
+++ spotify_deduper.py
@@ -1,7 +1,6 @@
 from typing import List, Tuple, Set
 
-from constants import INCLUDE_GENRES, EXCLUDE_GENRES, IDEAL_PLAYLIST_LENGTH, \
-    VALID_PLAYLIST_THRESHOLD
+from constants import INCLUDE_GENRES, EXCLUDE_GENRES
 from genre import Genre
 from playlist import Playlist
 from spotipy_wrapper import SpotipyWrapper
@@ -20,39 +19,34 @@ class SpotifyDeduper:
             for g in EXCLUDE_GENRES
         ]
     
-    def get_badly_labeled_playlists(self) -> List[Tuple[Playlist, str]]:
+    def get_badly_labeled_playlists(
+            self,
+            debug=False
+    ) -> List[Tuple[Playlist, str]]:
         valid_genre_names = INCLUDE_GENRES + EXCLUDE_GENRES
         playlists = self.wrapper.get_all_playlists()
         malformed_playlists = []
         
+        # DEBUG: use only test playlists.
+        if debug:
+            playlists = list(filter(
+                lambda p: 'TEST' in p.description, playlists
+            ))
+        
         for pl in playlists:
-            if not len(pl.name):
-                malformed_playlists.append((pl, 'NO PLAYLIST NAME'))
-                continue
-                
             if not len(pl.description):
                 malformed_playlists.append((pl, 'NO GENRE TAG '))
                 continue
             
-            name_tkn = pl.name.split(' ')
             description_tkn = pl.description.split(' ')
-            
             if description_tkn[0] not in valid_genre_names:
                 malformed_playlists.append((pl, 'BAD GENRE TAG'))
                 continue
-            
-            # Only query those that have passed all other filters
-            tracks = self.wrapper.get_all_tracks_from_playlist(pl.id)
-            pl.add_tracks(tracks)
-            
-            if name_tkn[0].isupper() and pl.length != IDEAL_PLAYLIST_LENGTH:
-                malformed_playlists.append((pl, 'BAD PLAYLIST NAME'))
-                continue
-            elif (name_tkn[0].islower() and
-                  pl.length >= VALID_PLAYLIST_THRESHOLD):
-                malformed_playlists.append((pl, 'BAD PLAYLIST NAME'))
-                continue
-
+                
+                # NOTE: Spotify does not allow playlists with empty names
+                name_tkn = pl.name.split(' ')
+                # TODO missing BAD PLAYLIST NAME (length checks)
+                
         return malformed_playlists
     
     def find_duplicates_single_playlist(self, playlist_id: str) -> Set[Track]:
--- spotify_deduper_tests.py
+++ spotify_deduper_tests.py
@@ -8,6 +8,22 @@ class SpotifyDeduperTests(TestCase):
     def setUp(self):
         self.deduper = SpotifyDeduper()
         
+    def test_get_badly_labeled_playlists(self):
+        # Manually trigger 'NO GENRE TAG' ()
+        # results = self.deduper.get_badly_labeled_playlists(debug=False)
+        # bad_playlist = results[0][0].name
+        # error_str = results[0][1]
+        # self.assertEqual(bad_playlist, TEST_PLAYLIST_1['name'])
+        # self.assertEqual(error_str, 'NO GENRE TAG')
+        
+        # Manually trigger 'BAD GENRE TAG'
+        # results = self.deduper.get_badly_labeled_playlists(debug=True)
+        # bad_playlist = results[0][0].name
+        # error_str = results[0][1]
+        # self.assertEqual(bad_playlist, TEST_PLAYLIST_1['name'])
+        # self.assertEqual(error_str, 'BAD GENRE TAG')
+        pass
+        
     def test_find_duplicates_single_playlist(self):
         # contains a duplicate
         dupes = self.deduper.find_duplicates_single_playlist(
